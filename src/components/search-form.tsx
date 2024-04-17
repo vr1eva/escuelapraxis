a6a5e52512e7a3fb691a92e6c915fca92481c09a
vr1 <hivr1eva@gmail.com>
2024-04-16 20:15:19 -0500

    fix menu spacing

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -8,8 +8,8 @@ import { cn } from "@/lib/utils"
 
 export default function SearchForm({ className }: SearchFormProps) {
     return (
-        <div className={cn("flex relative ml-auto", className)}>
-            <Input className="border-0 border-b-[1px]  border-b-dark rounded-none placeholder:text-slate-400 pl-[32px] " placeholder="Buscar..." />
+        <div className={cn("flex relative", className)}>
+            <Input className="border-0 border-b-[1px]  border-b-dark rounded-none placeholder:text-slate-400 pl-[32px]" placeholder="Buscar..." />
 
             <Image src={zoom} alt="search icon" width={24} height={24} className="absolute left-[5px] top-[5px]" />
         </div>
