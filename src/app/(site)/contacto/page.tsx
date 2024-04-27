4bc752722b7a23d2648a5d0282e2f797710478aa
vr1 <hivr1eva@gmail.com>
2024-04-27 18:08:07 -0500

    fix contact form

--- src/app/(site)/contacto/page.tsx
+++ src/app/(site)/contacto/page.tsx
@@ -19,9 +19,9 @@ export default function Page() {
 
     const contactInputClassName = cn(typographyBodyRegularClassNames, `text-gray w-full rounded-none border-dark p-[10px]`)
     const contactInfoHighlightClassName = "text-red font-semibold"
-    return <main className="min-h-screen">
-        <PageHeading className="max-w-[1160px]  mt-[152px] mx-auto">{data.heading}</PageHeading>
-        <div className="max-w-[1160px] mx-auto flex gap-[79px] mt-[32px]">
+    return <main className="min-h-screen px-6">
+        <PageHeading className="max-w-[1160px] mt-[32px] xl:mt-[152px] mx-auto">{data.heading}</PageHeading>
+        <div className="max-w-[1160px] mx-auto flex gap-[79px] mt-[32px] xl:flex-row flex-col">
             <div className="flex flex-col max-w-[506px] gap-[24px]">
                 <TypographyBodyRegular>Si deseas contactarnos, puedes dejarnos un mensaje a través del formulario o escribirnos directamente a <Link href="mailto:praxis@gmail.com" className={contactInfoHighlightClassName}>praxis@gmail.com</Link></TypographyBodyRegular>
                 <TypographyBodyRegular>Si estás interesado en comprar libros o consultar nuestro catálogo, puedes comunicarte al número <Link href="tel:+51987654321" className={contactInfoHighlightClassName}>987654321.</Link></TypographyBodyRegular>
@@ -30,13 +30,13 @@ export default function Page() {
                     <div className="shrink-0">
                         <Image width={24} height={24} src={facebook} alt="facebook logo" />
                     </div>
-                    <div className="shrink-0 ">
-                        <Image width={24} height={24} src={instagram} alt="instagram logo" />
+                    <div className="shrink-0">
+                        <Image className="mx-auto xl:mb-0 mb-[32px]" width={24} height={24} src={instagram} alt="instagram logo" />
                     </div>
                 </div>
                 <div className="shrink-0 mt-[40px] bg-blend-lighten"><Image className="object-cover" src={cosecha} alt="cosecha" width={172} height={192} /> </div>
             </div>
-            <div className="flex flex-col gap-[16px] pl-[79px] border-l border-l-light-gray">
+            <div className="flex flex-col gap-[16px] xl:pl-[79px] xl:border-l xl:border-l-light-gray -order-1 xl:order-1">
                 <TypographyBodyRegular className="w-full">Compártenos tus datos para que podamos ponernos en contacto contigo.</TypographyBodyRegular>
                 <Input className={cn(contactInputClassName)} placeholder="Nombres y Apellidos" />
                 <div className="flex gap-4">
